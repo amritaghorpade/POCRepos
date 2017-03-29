b4f496eac9a392e17899ac2de3de8af58638dc1c
amrita.ghorpade <amrita.ghorpade@irissoftware.com>
2017-03-29 12:51:13 +0530

    changed prop name

--- WebAPI/Models/Product.cs
+++ WebAPI/Models/Product.cs
@@ -12,6 +12,6 @@ namespace WebAPI.Models
         public string Category { get; set; }
         public decimal Price { get; set; }
 
-        public int IdTest { get; set; }
+        public int IdTestAdd { get; set; }
     }
 }
